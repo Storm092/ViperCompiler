508fbbc76d8f0e85cc8c579dfa82a0ff2859a17c
Storm <92469024+Storm092@users.noreply.github.com>
2021-10-16 09:02:34 +0200

    Add files via upload

---
+++ ViperCompiler/ViperCompiler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViperCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Compile
+            string exe = args[1];
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            CompilerParameters pars = new CompilerParameters();
+            pars.GenerateExecutable = true;
+            pars.OutputAssembly = exe;
+            pars.GenerateInMemory = false;
+            pars.TreatWarningsAsErrors = false;
+
+            CompilerResults result = provider.CompileAssemblyFromFile(pars, args[0]);
+
+            if (result.Errors.Count > 0)
+            {
+                foreach (CompilerError err in result.Errors)
+                {
+                    Console.WriteLine(err.ErrorText.ToString());
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
